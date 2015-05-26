6a5bc23243e1c664023cb0da0695fb3f9e2ffaba
Reyvilo <margerit.olivier@gmail.com>
2015-05-26 10:53:33 +0200

    YOLO

---
+++ TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            Console.WriteLine(currentProcess.Id);
+            Console.ReadKey();
+        }
+    }
+}
